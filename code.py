a09372e83a3031697a63565ae99aaa6bb2b88a7e
OmarShady1 <omarsjady@gmail.com>
2025-07-07 20:17:57 +0300

    draw

--- code.py
+++ code.py
@@ -5,7 +5,20 @@ HEIGHT = 600
 score = 0
 TITLE = "packman"
 player = Actor ("pacman")
+points = Actor ("points")
+game_over = False
 player.pos=500,400
+points.pos = 300,200
 def draw ():
-    screeen .clear()
-    
+    if not game_over:
+        screeen .clear()
+        screen .fill ((0,0,0))
+        player.draw
+        points.draw
+        screen .draw.text('score = '+str(score), (10,10))
+    else:
+        screen .clear()
+        screen.fill ("black")
+        screen.draw.text("gg",(200,200))
+        screen.draw.text("final score ="+str(score),(400,300))
+
